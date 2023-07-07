923b205583c275ddf93d018e85d4abba88bc468d
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-07-07 09:34:04 +0530

    added the vercel json rewrites for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar/NavBar';
 import Home from './pages/Home';
