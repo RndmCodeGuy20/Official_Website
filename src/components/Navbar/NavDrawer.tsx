df71cfcc6c2df3c0445a04ae0674b518a222341b
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-07-07 08:46:42 +0530

    added the nav drawer for mobile view

--- src/components/Navbar/NavDrawer.tsx
+++ src/components/Navbar/NavDrawer.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import close from '../../assets/nav/close.png'
 import styles from './NavDrawer.module.css'
 
 interface PROPS {
@@ -9,9 +10,20 @@ interface PROPS {
 const NavDrawer = (props) => {
 
   return (
-    <div className={styles.drawer}>
-
-    </div>
+    <ul className={styles.drawer}>
+        <div className={styles.close} onClick={() => {
+          props.setshowMenu(!props.showMenu)
+        }} >
+          <img src={close} width={25} alt="close menu button" />
+        </div>
+        <li><a href="/#">Home</a></li>
+        <li><a href="/#about">About</a></li>
+        <li onClick={() => {
+          props.setshowMenu(false)
+        }} ><a href="/team">Team</a></li>
+        <li><a href="/#achievements">Achievements</a></li>
+        <li><a href="/#faqs">FAQs</a></li>
+    </ul>
   )
 }
 
