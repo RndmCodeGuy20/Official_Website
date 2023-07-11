48a7f5dc21b54dc9888e9d7ae1abb6ebbbd2aa69
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-07-12 01:12:52 +0530

    updated the footer uI + made it responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,44 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import logo from '../../assets/logo.png'
+
 
 const Footer = () => {
   return (
-      <section className={styles["footer"]}>
+      <footer className={styles["footer"]}>
           <div className={styles["footer_container"]}>
               <div className={styles["footer_1"]}>
                   <a href="index.html"
                       className={styles["footer_logo"]}>
-                      <img src="assets/logo.png"
-                          width="250"
+                      <img src={logo}
+                          width="75%"
                           alt=""/>
                   </a>
               </div>
 
               <div className={styles["footer_2"]}>
-                  <h4>All Links</h4>
-                  <ul className="permalinks">
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#about">About</a></li>
-                      <li><a href="team.html">Team</a></li>
-                      <li><a href="contact.html">Contact</a></li>
+                <h4>All Links</h4>
+                  <ul className={styles["permalinks"]}>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/#about">About</a></li>
+                      <li><a href="/team">Team</a></li>
                   </ul>
               </div>
-
-              <div className={styles["footer_3"]}>
-                  <h4>Primary</h4>
-                  <ul>
-                      <li><a href="#">Privacy Policy</a></li>
-                      <li><a href="#">Contact Us</a></li>
-                      <li><a href="#">Refund Policy</a></li>
+              <div className={styles["footer_2"]}>
+                    <h4>Events</h4>
+                  <ul className={styles["permalinks"]}>
+                      <li><a href="">All Events</a></li>
+                      <li><a href="">Upcoming</a></li>
+                      <li><a href="">Recruitment</a></li>
                   </ul>
               </div>
 
               <div className={styles["footer_4"]}>
                   <h4>Contact Us</h4>
-                  <div>
-                      <p>+91 xxx xxxx xxx</p>
-                      <p>robionics@rknec.edu</p>
-                  </div>
+                  <ul >
+                      <li>+91 xxx xxxx xxx</li>
+                      <li>rac.club@rknec.edu</li>
+                  </ul>
                   <ul className={styles["footer_socials"]}>
                       <li><a href="#"><i className="uil uil-facebook-f"></i></a></li>
                       <li><a href="#"><i className="uil uil-instagram-alt"></i></a></li>
@@ -49,10 +49,10 @@ const Footer = () => {
 
           </div>
           <div className={styles["footer_copyright"]}>
-              <small>All rights owned by Robioncis
+              <small>All rights owned and reserved by <span>Robotics X Aviation Club</span>.
               </small>
           </div>
-      </section>
+      </footer>
   )
 }
 
