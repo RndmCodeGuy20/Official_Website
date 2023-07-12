aa1b2c34c1a8846d423784c56791302def4ad84c
yb <yagyeshsrbobde@gmail.com>
2023-07-12 08:28:41 +0530

    updated domain card structure

--- src/components/DomainCard/DomainCard.tsx
+++ src/components/DomainCard/DomainCard.tsx
@@ -4,24 +4,14 @@ import styles from './DomainCard.module.css'
 
 const DomainCard = (props) => {
   return (
-    <div className={styles["card"]}
-        style={{ 
-            background: `url(${props.domain.thumbnail})`,
-        }}
-    >
-        <div className={styles["card-content"]}>
-
-        <h2 className={styles["card-title"]}>
-            Awesome
-        </h2>
-        <p className={styles["card-body"]}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis facilis ullam animi? Perspiciatis assumenda quia dolorum consectetur atque temporibus ex officia porro odit eligendi ratione, at expedita quis exercitationem sint.
-        </p>
-        <a href="#" className="button">
-            Learn More
-        </a>
-        </div>
+    <div className="box">
+    <span></span>
+    <div className="content">
+      <h2>Card one</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <a href="#">Read More</a>
     </div>
+  </div>
   )
 }
 
